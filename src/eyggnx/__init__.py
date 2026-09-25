a86f8dc614a986d63bd1e70fc9f9c32db70798ed
SHAR-K <ghasindusk@gmail.com>
2026-09-25 13:42:59 +0900

    feat: publish GENESIS source and GitHub workflow

---
+++ src/eyggnx/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026 SHAR-K
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+"""EYGGDRANEX (EYGGNX) — Open-Ended Artificial Digital Life Ecosystem."""
+
+__version__ = "0.1.0a1"
